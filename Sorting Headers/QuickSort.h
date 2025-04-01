b81384466d38c7f4c0d45fa7acabf3f5427046ef
GJ-Eclipse <gjla2023-7083-62053@bicol-u.edu.ph>
2025-04-01 14:53:37 +0800

    Create QuickSort.h

---
+++ Sorting Headers/QuickSort.h
@@ -0,0 +1,25 @@
+int partition(int array[], int start, int end){
+
+    int pivot = array[end];
+    int i = start-1;
+
+    for (int j = start; j < end; j++){
+        if (array[j]<pivot){
+            i++;
+            swap(&array[i],&array[j]);
+        }
+    }
+
+    swap(&array[i+1],&array[end]);
+    return (i+1);
+}
+
+void quickSort(int array[], int low, int high) {
+
+    if (low < high) {
+        int p = partition(array, low, high);
+
+        quickSort(array, low, p - 1);
+        quickSort(array, p + 1, high);
+    }
+}
