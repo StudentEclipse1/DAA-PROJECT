b5a5c1d0761a01ba0dace6a784fd8ef6a56a0532
Raymond <168504854+Ry-21@users.noreply.github.com>
2025-04-01 20:00:52 +0800

    Add files via upload

---
+++ Sorting Headers/BubbleSort.h
@@ -0,0 +1,32 @@
+// # Raymond C. Balingbing
+// # DAA - BSCS-2B
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void modifiedBubbleSort(int arr[], int n) {
+    int flag;
+    for (int i = 0; i < n - 1; i++) {
+        flag = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = 1;
+            }
+        }
+        if (flag == 0) {
+            break;
+        }
+    }
+}
