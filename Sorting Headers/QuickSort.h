a27fde079dee4458f7e14327d3c837cc7e886671
GJ-Eclipse <gjla2023-7083-62053@bicol-u.edu.ph>
2025-04-01 19:56:11 +0800

    Added medianOfThree to QuickSort.h

--- Sorting Headers/QuickSort.h
+++ Sorting Headers/QuickSort.h
@@ -1,4 +1,20 @@
 extern void swap(int *a, int *b);
+
+int medianOfThree(int array[], int low, int high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[low] > array[mid])
+        swap(&array[low], &array[mid]);
+    if (array[low] > array[high])
+        swap(&array[low], &array[high]);
+    if (array[mid] > array[high])
+        swap(&array[mid], &array[high]);
+
+    swap(&array[mid], &array[high]);
+    return array[high];
+}
+
+
 int partition(int array[], int start, int end){
 
     int pivot = array[end];
@@ -18,6 +34,7 @@ int partition(int array[], int start, int end){
 void quickSort(int array[], int low, int high) {
 
     if (low < high) {
+        medianOfThree(array, low, high);
         int p = partition(array, low, high);
 
         quickSort(array, low, p - 1);
