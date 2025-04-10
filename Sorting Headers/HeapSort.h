544a6285fe2d829c3be94f4345f71203f680dede
JuanMBZ <juanmiguelzurbito12@gmail.com>
2025-04-10 12:33:55 +0800

    fix heapsort

--- Sorting Headers/HeapSort.h
+++ Sorting Headers/HeapSort.h
@@ -20,7 +20,7 @@ void maxHeapify(int array[], int parentIndex, int length){
 }
 
 void buildMaxHeap(int array[], int n){
-    for(int i = (n/2)-1; i>=0; i--){
+    for(int i = (n-1)/2; i>=0; i--){
         maxHeapify(array, i, n);
     }
 }
